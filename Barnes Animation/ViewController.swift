acd61ee86cd5aed676b26243a3e7910dd7e47933
Ben Barnes <bm.barnes@setonhill.edu>
2020-02-23 16:01:58 -0500

    Initial Commit

---
+++ Barnes Animation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Barnes Animation
+//
+//  Created by ben barnes on 2/23/20.
+//  Copyright © 2020 ben barnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
